49b46c7a54cf1339fcde259550fa041c1466d2c1
Vladimir Revyagin <skorabeyf@gmail.com>
2024-06-24 12:33:16 +0300

    start development useFetch

---
+++ src/Demo.jsx
@@ -0,0 +1,29 @@
+import useFetch from "./hook/useFetch";
+
+export default function Demo() {
+  // const {
+  //   data,
+  //   isLoading,
+  //   error,
+  //   refetch
+  // } = useFetch('https://jsonplaceholder.typicode.com/post');
+
+  return (
+    <div>
+      <div>
+        <button
+        // onClick={() => refetch({
+        //   params: {
+        //     _limit: 3
+        //   }
+        // })}
+        >
+          Перезапросить
+        </button>
+      </div>
+      {/* {isLoading && 'Загрузка...'} */}
+      {/* {error && 'Произошла ошибка'} */}
+      {/* {data && !isLoading && data.map(item => <div key={item.id}>{item.title}</div>)} */}
+    </div>
+  );
+}
